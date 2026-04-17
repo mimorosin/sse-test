698c10b18b9a15ab2819b14b1ec44dc6a5a909b0
Canc <krya@nate.com>
2026-04-17 15:51:19 +0900

    Revert "patch 테스트"
    This reverts commit b28837e0656b8b06177ea94635212b66b618bfac.

--- ab_custom_directory/a/PlyLoader.cpp
+++ ab_custom_directory/a/PlyLoader.cpp
@@ -567,10 +567,7 @@ void PLYImporter::LoadFace(const PLY::Element *pcElement, const PLY::ElementInst
         if (mGeneratedMesh->mFaces == nullptr) {
             mGeneratedMesh->mNumFaces = pcElement->NumOccur;
             mGeneratedMesh->mFaces = new aiFace[mGeneratedMesh->mNumFaces];
-        } else {
-             if (mGeneratedMesh->mNumFaces < pcElement->NumOccur) {
-                 throw DeadlyImportError("Invalid .ply file: Too many faces");
-             }
+        }
 
         if (!bIsTriStrip) {
             // parse the list of vertex indices
